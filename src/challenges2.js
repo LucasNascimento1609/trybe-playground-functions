511672b9e68cb42524c2ac936e77a0c6cdcaac08
lucas <lucasnascimento.lp@gmail.com>
2021-12-03 20:56:31 -0300

    Desafio 10 OK

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,21 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(techNames, name) {
+  let orderedTechNames = techNames.sort();
+  let techList = [];
+
+  for (let i = 0; i < techNames.length; i++) {
+    let techItem = {};
+    techItem.tech = techNames[i];
+    techItem.name = name;
+    techList.push(techItem);
+  }
+
+  if (techNames.length > 0) {
+    return techList;
+  }
+  return 'Vazio!';
+
+  // Encontrei o .sort aqui: https://www.edsonemiliano.com.br/blog/como-ordenar-uma-array-de-objetos-com-javascript-sort/#:~:text=Caso%20voc%C3%AA%20tenha%20um%20array,usar%20o%20m%C3%A9todo%20sort().&text=return%20(a.,nome%20%3E%20b.
 }
 
 // Desafio 11
