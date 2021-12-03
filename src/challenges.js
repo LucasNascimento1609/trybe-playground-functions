4d4d7494b5e330ee5711db4476b52bc6f7168eea
lucas <lucasnascimento.lp@gmail.com>
2021-12-02 22:23:05 -0300

    Atualiza Desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -75,7 +75,8 @@ function catAndMouse(mouse, cat1, cat2) {
 
   if (distanceCat1 < distanceCat2) {
     return 'cat1';
-  } else if (distanceCat2 < distanceCat1) {
+  }
+  if (distanceCat2 < distanceCat1) {
     return 'cat2';
   }
   return 'os gatos trombam e o rato foge';
