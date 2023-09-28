a61f783d9e9906bd0f87e9960e20508ee6ffba2f
Lucas Nascimento <lucasnascimento.lp@gmail.com>
2023-09-28 16:39:32 -0300

    refactor: :recycle: Refact Desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,21 +1,8 @@
 // Desafio 10
-function techList(techNames, name) {
-  let orderedTechNames = techNames.sort();
-  let techList = [];
+function techList(techs, name) {
+  if (!techs.length) return 'Vazio!';
 
-  for (let i = 0; i < orderedTechNames.length; i++) {
-    let techItem = {};
-    techItem.tech = techNames[i];
-    techItem.name = name;
-    techList.push(techItem);
-  }
-
-  if (techNames.length > 0) {
-    return techList;
-  }
-  return 'Vazio!';
-
-  // Encontrei o .sort aqui: https://www.edsonemiliano.com.br/blog/como-ordenar-uma-array-de-objetos-com-javascript-sort/#:~:text=Caso%20voc%C3%AA%20tenha%20um%20array,usar%20o%20m%C3%A9todo%20sort().&text=return%20(a.,nome%20%3E%20b.
+  return techs.sort().map((tech) => ({ tech, name }));
 }
 
 // Desafio 11
